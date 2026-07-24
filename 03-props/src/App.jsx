7c984d686c614c3014151c06234febe589c2b6a8
shubhamsharmayo <shubham2321087@gmail.com>
2026-07-24 10:37:55 +0530

    Added Card component and updated props example

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className='parent ' >
-      <div className="card">
-        <img src="https://images.unsplash.com/photo-1782796474844-92cc237e9550?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Aditya Singh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <button>View Profile</button>
-      </div>
+      <Card user='Aditya' age={18} img='https://images.unsplash.com/photo-1784744847844-7990d7da0e15?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user='Bob' age={22} img='https://images.unsplash.com/photo-1784652587186-868e0206b64f?q=80&w=1166&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user='Charlie' age={25} img='https://images.unsplash.com/photo-1779896412071-bf3160aad19e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     </div>
   )
 }
